947f77a9634c161d8afbd2dd7142ebda260b3a02
shtyagi22 <sh.tyagi22@gmail.com>
2022-10-30 16:20:17 -0400

    database set up

--- server/app.js
+++ server/app.js
@@ -5,6 +5,9 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+const { request } = require('express');
+const cors = require('cors');
+
 
 var app = express();
 
@@ -12,9 +15,23 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(cors()) // CORS middleware useage
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+const APP_id = "e7215228";
+const APP_key = "2f5c8ccb5407391f803663f04d6a1671";
+
+app.get('/api', (req, res) => 
+
+  request.get(`https://api.spoonacular.com/recipes/findByIngredients?apiKey=73760b73b3fd4ae1ac47f74ebd4ac47c&ingredients=apples,+flour,+sugar&number=2`, (error, response) => {
+    res.send(JSON.parse(response.body));
+  }
+  )
+  // const cats = ["smudge", "mama", "fritz"];
+  // res.json(cats);
+
+);
 
 module.exports = app;
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,9 +1,14 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const users = require('../db/queries/users');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+
+
+router.get('/', (req, res) => {
+  users.getAllUsers().then(data => {
+    console.log(data);
+    res.json({users: data});
+  })
 });
 
 module.exports = router;
