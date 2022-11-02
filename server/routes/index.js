3b7146ec67035537b5bd2a95487b4f9585571bd6
shtyagi22 <sh.tyagi22@gmail.com>
2022-11-02 19:44:59 -0400

    send back api response based on the search query recieved from front end

--- server/routes/index.js
+++ server/routes/index.js
@@ -11,24 +11,44 @@ router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.put('/api', (req, res) => {
+router.put('/api', async (req, res) => {
   const data = req.body;
   console.log("WE ARE IN THE SERVER", data);
-  res.json(data)
+  const fetch_ingredients = function (arr) {
+    let ingredients = [];
+    let string = "";
+    for (let i of arr) {
+      ingredients.push(i.ingredient)
+    }
+    console.log("ingredients array:", ingredients);
+    for (let ing = 0; ing < ingredients.length; ing++) {
+      string += ingredients[ing] + ",+";
+
+    }
+    return string;
+  }
+  const params = fetch_ingredients(data);
+  const APP_id = "e7215228";
+  const APP_key = "2f5c8ccb5407391f803663f04d6a1671";
+  const response = await fetch(`https://api.edamam.com/search?app_id=${APP_id}&app_key=${APP_key}&q=${params}`);
+  const body = await response.json()
+  console.log("response", body);
+  res.json(body);
 });
 
 
 
-const APP_id = "e7215228";
-const APP_key = "2f5c8ccb5407391f803663f04d6a1671";
-
-router.get('/api', async (req, res) => {
-  const response = await fetch(`https://api.spoonacular.com/recipes/findByIngredients?apiKey=73760b73b3fd4ae1ac47f74ebd4ac47c&ingredients=apples,+flour,+sugar&number=2`);
-  const data = await response.json();
-  res.send(data);
 
 
-});
+
+//https://api.spoonacular.com/recipes/findByIngredients?apiKey=73760b73b3fd4ae1ac47f74ebd4ac47c&ingredients=apples,+flour,+sugar&number=2
+// router.get('/api', async (req, res) => {
+//   const response = await fetch(`https://api.edamam.com/search?app_id=${APP_id}&app_key=${APP_key}&q=apples,+flour,+sugar,+`);
+//   const data = await response.json();
+//   res.send(data);
+
+
+// });
 
 
 module.exports = router;
