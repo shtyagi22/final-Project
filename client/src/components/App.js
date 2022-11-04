a5d797da241f4c017c081cb9b3aa5da31a40b574
shtyagi22 <sh.tyagi22@gmail.com>
2022-11-04 01:20:42 -0400

    made some changes to recieve user data from server

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -15,25 +15,12 @@ import LoginOptions from './LoginOptions';
 import RecipeDetail from './RecipeDetail';
 import Feeds from './Feeds';
 import RecipeCardItems from './RecipeCardItems';
-import {BrowserRouter,Route,Routes} from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from './Home';
 import PreviousPost from './PreviousPost';
 
 
 function App() {
-
-
-
-  const handleCallbackResponse = (response) => {
-    console.log("credential", response.credential)
-    const userObject = jwt_decode(response.credential);
-    console.log(userObject)
-  }
-
-  useEffect(() => {
-    axios.get("/api").then((res) => [
-      console.log(res.data.hits)
-    ], [])
 
   const [recipes, setRecipes] = useState([]);
 
@@ -46,42 +33,17 @@ function App() {
   //     setRecipes(res)
   //   ])
 
-
   // },[])
   const user = JSON.parse(localStorage.getItem("user"))
-  console.log(user)
-
-  function searchIngredients(arr_ingrediends){
-    console.log(arr_ingrediends)
-  
-  return axios.put("/api", arr_ingrediends).then((res)=>{
-    return res.data
-  })
-}
-
-
-  useEffect(() => {
-    /* global google */
-    google.accounts.id.initialize({
-      client_id: "354838477230-p6j3da9j9fl49g4r1om2qn8l51ierbmh.apps.googleusercontent.com",
-      callback: handleCallbackResponse
-    })
-
-    google.accounts.id.renderButton(
-      document.getElementById("signInDiv"),
-      { theme: "outline", size: "large" }
-    )
-  }, [])
 
   function searchIngredients(arr_ingrediends) {
     console.log(arr_ingrediends)
 
-    return axios.put("/api", arr_ingrediends).then((data) => {
-      console.log("data from server:", data);
-      return data;
-
+    return axios.put("/api", arr_ingrediends).then((res) => {
+      return res.data
     })
   }
+
 
 
   const recipeDescription = {
@@ -95,7 +57,6 @@ function App() {
     description: "With just a couple of avocados and some pantry staples, you can approximate the flavors and textures of creamy, delicate slices of sashimi anytime, even when you have no fresh fish on hand. With a salty-sweet sauce and a dash of spicy shichimi togarashi, this paleo and vegan sashimi is delicious enough for eaters of all stripes.",
     ingredients: ["2 Hass avocados", "2 Tbsp. coconut aminos", "2 tsp. furikake ", "1/4 tsp. shichimi togarashi", "1 tsp. flake sea salt", "2 scallions"]
   }
-
 
 
 
@@ -123,144 +84,93 @@ function App() {
       }
     ]
 
-  const [nav, setNav] = useState('');
-
-  const navigate = (navigation) => {
-    setNav(navigation)
-
-  const comments = 
-  [
+  const [posts, setPosts] = useState([
     {
-    id:1,
-    comment: {
-      image: "https://lh3.googleusercontent.com/a/ALm5wu1_-dgWtl7-p3AMWUcgUpYnJAV_zG0iMe59OOaH=s112-c-rw-v1-e365",
-      name: "Erica M.",
-      time: "2 years ago",
-      rating:4.5,
-      comment: "I agree with the other post about the dish needing more seasoning. I added additional seasoning but it still didn't do much for the flavor. The topping was also a little dry."
+      id: 1,
+      username: "Michelle",
+      userImage: "https://graph.facebook.com/10208015133285596/picture?height=180&width=180",
+      post: "I’m so proud and lucky to be a native Californian because amazing produce is available at my local grocery store year-round. I didn’t even realize until recently that California grows more than a third of the country’s vegetables and two-thirds of the country’s",
+      postImage: "https://lh3.googleusercontent.com/RBFG1uWmfwY6gJEhEdGePV6mjXv4E5vdHhPFuuRsa59PRDtEomGu8WVD8VxGBhgmDE9EwYPWYt1UJIP-w54hC8slMBQI1p0hZtzW=w1280-h1280-c-rw-v1-e365"
+    },
+    {
+      id: 2,
+      username: "Michelle",
+      userImage: "https://graph.facebook.com/10208015133285596/picture?height=180&width=180",
+      post: " made half the recipe and substituted half and half for the heavy cream… also used minced garlic from a jar and it was amazing!!! I will be making this more often for sure and save as a favorite!",
+      postImage: null
     }
-  },
-  {
-    id:2,
-    comment: {
-      image: "https://graph.facebook.com/10156755061204968/picture?height=180&width=180",
-      name: "Nicole Wall",
-      time: "10 months ago",
-      rating:3,
-      comment: "i didn't look anything like the picture i might have done something wrong but it was still good."
+  ])
+
+
+
+  const handleUser = (newUser) => {
+    console.log(newUser)
+
+    axios.post('/users', newUser).then((res) => {
+      console.log("response", res);
+      return res
+    })
+
+  }
+
+  const handleComment = (comment) => {
+    comment.user = { user }
+    console.log(comment)
+    axios.post('/comments', comment).then((res) => {
+      return res
+    })
+
+  }
+  const handlePosts = (newPost) => {
+    const newPostTobeAdded = {
+      id: 3,
+      username: "Michelle",
+      userImage: "https://graph.facebook.com/10208015133285596/picture?height=180&width=180",
+      post: newPost.postMessage,
+      postImage: newPost.postImage
     }
+    const filename = newPost.postImage.name
+    const file = newPost.postImage;
+    const text = newPost.postMessage
+
+    const formData = new FormData();
+    formData.append('text', text)
+    formData.append('upload', file)
+    console.log(formData)
+
+    axios.post('/posts', {
+      headers: { 'Content-Type': 'multipart/form-data' },
+      body: formData,
+    });
+
+    setPosts(prev => [newPostTobeAdded, ...prev])
   }
-]
-
-const [posts,setPosts] = useState([
-  {
-    id:1,
-    username: "Michelle",
-    userImage:"https://graph.facebook.com/10208015133285596/picture?height=180&width=180",
-    post: "I’m so proud and lucky to be a native Californian because amazing produce is available at my local grocery store year-round. I didn’t even realize until recently that California grows more than a third of the country’s vegetables and two-thirds of the country’s",
-    postImage:"https://lh3.googleusercontent.com/RBFG1uWmfwY6gJEhEdGePV6mjXv4E5vdHhPFuuRsa59PRDtEomGu8WVD8VxGBhgmDE9EwYPWYt1UJIP-w54hC8slMBQI1p0hZtzW=w1280-h1280-c-rw-v1-e365"
-  },
-  {
-    id:2,
-    username: "Michelle",
-    userImage:"https://graph.facebook.com/10208015133285596/picture?height=180&width=180",
-    post: " made half the recipe and substituted half and half for the heavy cream… also used minced garlic from a jar and it was amazing!!! I will be making this more often for sure and save as a favorite!",
-    postImage:null
-  }
-])
-
-
-
-const handleUser = (newUser) => {
-  console.log(newUser)
-
-  axios.post('/users', newUser).then((res)=>{
-    return res
-  })
-
-}
-
-const handleComment = (comment) => {
-  comment.user = {user}
-  console.log(comment)
-  axios.post('/comments', comment).then((res)=>{
-    return res
-  })
-
-}
-const handlePosts = (newPost) => {
-  const newPostTobeAdded = {
-    id:3,
-    username:"Michelle",
-    userImage:"https://graph.facebook.com/10208015133285596/picture?height=180&width=180",
-    post: newPost.postMessage,
-    postImage: newPost.postImage.image
-
-  }
-  axios.post('/posts', newPost).then((res)=>{
-    return res
-  })
-
-  setPosts(prev => [newPostTobeAdded, ...prev])
-}
 
   return (
     <main className="layout">
       <section className="sidebar">
-        <img
-          className="sidebar--centered"
-          src="images/food.png"
-          alt="Interview Scheduler"
-        />
-        <SignUpLogInButton />
-        <div className="sidebar--centered pantry" onClick={() => navigate("PANTRY")}>
-          Pantry Ready Recipe
-        </div>
-      </section>
-      {nav === "PANTRY" &&
-        <section className="schedule_pantry">
-          <PantryReady searchIngredients={searchIngredients} />
-        </section>
-      }
-      {nav === "" &&
-        <section className="schedule">
-          {/* <SearchBar/>
-        <RecipeCard/>
-        <SignUpLogInPage/>
-        <Post/> */}
-
-          <RecipeDetail
-            recipe={recipeDescription}
-            comments={comments}
-          />
-          <div className='App'>
-            <div id='signInDiv'></div>
-          </div>
-        </section>
-      }
 
       </section>
-        <section className="main_side">
-          <BrowserRouter>
-            <Routes>
-              <Route path='/' element={<>
-              <Home/>
-              <RecipeCardItems recipes={recipes}/>
-              </>}/>
-              <Route path='/login' element={<LoginOptions handleUser={handleUser}/>}/>
-              <Route path='/feeds' element={<Feeds onNewPost={handlePosts} posts={posts}/>}/>
-              <Route path='/signin' element={<SignUpLogInPage handleUser={handleUser}/>}/>
-              <Route path='/search_pantry_ingredients' 
-              element={<PantryReady searchIngredients={searchIngredients}/> }/>
-              <Route path='/recipe_details' element={<RecipeDetail recipe={recipeDescription} comments={comments} 
-              onComment={handleComment}/>}/>
-            </Routes>
-          </BrowserRouter>
-        </section>
-
+      <section className="main_side">
+        <BrowserRouter>
+          <Routes>
+            <Route path='/' element={<>
+              <Home />
+              <RecipeCardItems recipes={recipes} />
+            </>} />
+            <Route path='/login' element={<LoginOptions handleUser={handleUser} />} />
+            <Route path='/feeds' element={<Feeds onNewPost={handlePosts} posts={posts} />} />
+            <Route path='/signin' element={<SignUpLogInPage handleUser={handleUser} />} />
+            <Route path='/search_pantry_ingredients'
+              element={<PantryReady searchIngredients={searchIngredients} />} />
+            <Route path='/recipe_details' element={<RecipeDetail recipe={recipeDescription} comments={comments}
+              onComment={handleComment} />} />
+          </Routes>
+        </BrowserRouter>
+      </section>
 
     </main>
-  
+
 
   );
 
