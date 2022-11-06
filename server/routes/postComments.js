51c87dd0903b4ccd2dba2e907ff13899e982ae20
shtyagi22 <sh.tyagi22@gmail.com>
2022-11-06 01:22:24 -0500

    implementing new feature for posts replies

--- server/routes/postComments.js
+++ server/routes/postComments.js
@@ -4,11 +4,31 @@ const postComments = require('../db/queries/postComments');
 
 
 
-router.get('/', (req, res) => {
-  postComments.getAllPostComments().then(data => {
-    console.log(data);
-    res.json({ postComments: data });
-  })
+// router.get('/', (req, res) => {
+//   postComments.getAllPostComments().then(data => {
+//     console.log(data);
+//     res.json({ postComments: data });
+//   })
+// });
+
+router.get('/:id', (req, res) => {
+  console.log("req.params.id:", req.params.id)
+  const postId = req.params.id;
+  console.log("postId:", postId)
+  comments.getPostCommentbyPostId(postId)
+    .then((data) => {
+      console.log("data returned from getPostCommentbyPostId:", data)
+      res.json(data);
+    });
+});
+
+router.post("/", (req, res) => {
+
+  comments.createComments(req.body)
+    .then(data => {
+      console.log("data returned from createComments:", data)
+
+    });
 });
 
 module.exports = router;
